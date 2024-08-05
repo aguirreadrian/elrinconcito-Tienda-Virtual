d56d771eb7dec5e7c760db7f92e7280dc0f83946
Adrián Aguirre <107267038+aguirreadrian@users.noreply.github.com>
2024-08-05 06:59:32 -0300

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,41 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">¿Por qué elegir 'El Rinconsito'?</h4>
+                <p> 
+                   En 'El Rinconsito', creemos que los libros son más que simples objetos; son puertas a otros mundos, compañeros de viaje y maestros silenciosos. Aquí, te ofrecemos una experiencia literaria única que va más allá de la simple compra de un libro.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Dirección</h4>
+                <p style="color: #FFF">Reconquista 520, Maipú</p>
+                <p style="color: #FFF">Mendoza</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true"> (261)3385191</p> <!--<a href="#" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a>--></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; elrinconsito@gmail.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">ElRinconsito &copy; 2021</h5>
+</footer>
